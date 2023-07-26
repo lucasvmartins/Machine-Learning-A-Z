9b69725c6978d29a3c512a288018b2e11169f6a6
Lucas Martins <lucasvm20@hotmail.com>
2023-07-25 22:09:17 -0300

    1. Data Preprocessing
    Creating folders, files and importing libraries and the dataset to start the data preprocessing

---
+++ Machine Learning A-Z™ - AI, Python & R + ChatGPT Bonus [2023]/1. Data Preprocessing/Data Preprocessing.py
@@ -0,0 +1,13 @@
+#################### Pré-processamento de Dados ####################
+
+# %%
+# Importing the libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# %%
+# Loading the dataset
+data = pd.read_csv('Dataset/Data.csv')
+
+# %%
