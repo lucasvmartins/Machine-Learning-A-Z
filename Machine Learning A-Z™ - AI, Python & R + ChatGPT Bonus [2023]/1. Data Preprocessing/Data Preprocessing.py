2f9fa61da9cacf780680fb03561e9e26d70f3fe3
Lucas Martins <lucasvm20@hotmail.com>
2023-08-08 00:01:02 -0300

    Taking care of missing data
    Transforming the missing data into the average of the data in the same column

--- Machine Learning A-Z™ - AI, Python & R + ChatGPT Bonus [2023]/1. Data Preprocessing/Data Preprocessing.py
+++ Machine Learning A-Z™ - AI, Python & R + ChatGPT Bonus [2023]/1. Data Preprocessing/Data Preprocessing.py
@@ -23,6 +23,8 @@ y = data.iloc[:, -1].values
 from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 
+imputer.fit(x[:, 1:3])
+x[:, 1:3] = imputer.transform(x[:, 1:3])
 
 # %%
 
