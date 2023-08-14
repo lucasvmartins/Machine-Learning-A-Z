0175f13d62976711683d89f1158378438264c048
Lucas Martins <lucasvm20@hotmail.com>
2023-08-13 22:38:07 -0300

    Encoding Categorical Data
    Encoding Categorical Data: Encoding the independent variable and Encoding the dependent variable

--- Machine Learning A-Z™ - AI, Python & R + ChatGPT Bonus [2023]/1. Data Preprocessing/Data Preprocessing.py
+++ Machine Learning A-Z™ - AI, Python & R + ChatGPT Bonus [2023]/1. Data Preprocessing/Data Preprocessing.py
@@ -2,14 +2,14 @@
 
 
 # %%
-#################### Importing the libraries ####################
+#################### Importing The Libraries ####################
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 # %%
-#################### Importing the dataset ####################
+#################### Importing The Dataset ####################
 
 data = pd.read_csv('Dataset/Data.csv')
 
@@ -19,12 +19,29 @@ y = data.iloc[:, -1].values
 
 
 # %%
-#################### Taking care of missing data ####################
+#################### Taking Care of Missing Data ####################
 from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 
 imputer.fit(x[:, 1:3])
 x[:, 1:3] = imputer.transform(x[:, 1:3])
 
-# %%
 
+# %%
+#################### Encoding Categorical Data ####################
+
+# Encoding the independent variable
+
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+
+x = np.array(ct.fit_transform(x))
+
+# %%
+# Encoding the dependent variable
+from sklearn.preprocessing import LabelEncoder
+
+le = LabelEncoder()
+y = le.fit_transform(y)
