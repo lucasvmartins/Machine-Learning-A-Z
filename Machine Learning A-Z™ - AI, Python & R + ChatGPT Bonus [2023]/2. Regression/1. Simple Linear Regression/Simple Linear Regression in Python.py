82df170d012f68977cb83974418427df5eadf325
Lucas Martins <lucasvm20@hotmail.com>
2023-10-26 18:26:21 -0300

    Getting Started
    Getting started with the first steps: Importing the Libraries and Importing the Dataset

--- Machine Learning A-Z™ - AI, Python & R + ChatGPT Bonus [2023]/2. Regression/1. Simple Linear Regression/Simple Linear Regression in Python.py
+++ Machine Learning A-Z™ - AI, Python & R + ChatGPT Bonus [2023]/2. Regression/1. Simple Linear Regression/Simple Linear Regression in Python.py
@@ -3,12 +3,19 @@
 #%%
 # -------------------- Importing the Libraries --------------------
 import pandas as pd
+import numpy as np
+
 
 
 #%%
 # -------------------- Importing the Dataset --------------------
 data = pd.read_csv('Dataset/Salary_Data.csv')
 
+# Check for missing values
+
+# Separating feature values from the dependent variable
+X = data.iloc[:, :-1]
+y = data.iloc[:, -1]
 
 #%%
 # ---------- Splitting the Dataset Into the Training and Test Set ----------
