c7c5c21ddaacf47bd2e47cd844c7461839ced47a
Lucas Martins <lucasvm20@hotmail.com>
2023-10-26 18:13:51 -0300

    Getting Started
    Getting started with the first steps

---
+++ Machine Learning A-Z™ - AI, Python & R + ChatGPT Bonus [2023]/2. Regression/1. Simple Linear Regression/Simple Linear Regression in Python.py
@@ -0,0 +1,14 @@
+# -------------------- Simple Linear Regression in Python  --------------------
+
+#%%
+# -------------------- Importing the Libraries --------------------
+import pandas as pd
+
+
+#%%
+# -------------------- Importing the Dataset --------------------
+data = pd.read_csv('Dataset/Salary_Data.csv')
+
+
+#%%
+# ---------- Splitting the Dataset Into the Training and Test Set ----------
