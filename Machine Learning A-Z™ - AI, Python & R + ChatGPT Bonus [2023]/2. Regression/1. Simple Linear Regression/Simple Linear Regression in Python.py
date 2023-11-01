83f9fa98c756d4c37bd768d9ab33a66e849d5486
Lucas Martins <lucasvm20@hotmail.com>
2023-11-01 00:39:41 -0300

    Splitting the Dataset Into Training and Test Set
    Splitting the dataset into the training and test set

--- Machine Learning A-Z™ - AI, Python & R + ChatGPT Bonus [2023]/2. Regression/1. Simple Linear Regression/Simple Linear Regression in Python.py
+++ Machine Learning A-Z™ - AI, Python & R + ChatGPT Bonus [2023]/2. Regression/1. Simple Linear Regression/Simple Linear Regression in Python.py
@@ -4,7 +4,9 @@
 # -------------------- Importing the Libraries --------------------
 import pandas as pd
 import numpy as np
-
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 
 #%%
@@ -17,5 +19,13 @@ data = pd.read_csv('Dataset/Salary_Data.csv')
 X = data.iloc[:, :-1]
 y = data.iloc[:, -1]
 
+
 #%%
 # ---------- Splitting the Dataset Into the Training and Test Set ----------
+X_train, X_test, y_train,  y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+
+#%%
+# -------------------- Importing the Dataset --------------------
+regressor = 
+
