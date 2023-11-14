dd494e77d892c62e57cc3f839b2d9025905975ff
Lucas Martins <lucasvm20@hotmail.com>
2023-11-13 22:26:08 -0300

    Predicting the Test set Results
    Predicting the Test set Results

--- Machine Learning A-Z™ - AI, Python & R + ChatGPT Bonus [2023]/2. Regression/1. Simple Linear Regression/R/Simple Linear Regression in R.R
+++ Machine Learning A-Z™ - AI, Python & R + ChatGPT Bonus [2023]/2. Regression/1. Simple Linear Regression/R/Simple Linear Regression in R.R
@@ -19,3 +19,18 @@ test_set = subset(data, split == FALSE)
 # ---------- Fitting Simple Linear Regression to the Training set ----------
 regressor = lm(formula = Salary ~ YearsExperience,
                data = training_set)
+
+
+# ---------- Predicting the Test set Results ----------
+y_pred = predict(regressor, newdata = test_set)
+# install.packages("ggplot2")
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = training_set$YearsExperience, y = training_set$Salary),
+             colour = 'red') +
+  geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
+            colour = 'blue') +
+  ggtitle('Salary per Years of Experience (Training set)') +
+  xlab('Years of Experience') +
+  ylab('Salary')
+
